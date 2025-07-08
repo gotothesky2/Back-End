8253d6aafcb63cf468be6a417374d9bf1b4fefd6
MinJunKKang <kmg4613@naver.com>
2025-07-08 19:24:58 +0900

    feat: initial Spring Boot Project

---
+++ src/main/java/hackerthon/likelion13th/canfly/CanflyApplication.java
@@ -0,0 +1,13 @@
+package hackerthon.likelion13th.canfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackerthon/likelion13th/canfly/CanflyApplicationTests.java
@@ -0,0 +1,13 @@
+package hackerthon.likelion13th.canfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
