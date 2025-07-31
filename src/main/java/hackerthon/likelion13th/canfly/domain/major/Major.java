93af8fa0d5be76e788cd0586e9680135112bb5bd
MinJunKKang <kmg4613@naver.com>
2025-07-31 21:01:22 +0900

    Major, University domain 관계 추가

--- src/main/java/hackerthon/likelion13th/canfly/domain/major/Major.java
+++ src/main/java/hackerthon/likelion13th/canfly/domain/major/Major.java
@@ -2,26 +2,36 @@ package hackerthon.likelion13th.canfly.domain.major;
 
 import hackerthon.likelion13th.canfly.domain.entity.BaseEntity;
 import hackerthon.likelion13th.canfly.domain.field.Field;
+import hackerthon.likelion13th.canfly.domain.university.University;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "major")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Major extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "mId")
     private Long id;
 
     @Column(name = "mName", length = 50, nullable = false)
     private String name;
 
+    @Column(name = "mCode", nullable = false)
+    private Integer code;
+
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "fId")
     private Field field;
+
+    @Builder.Default
+    @ManyToMany(mappedBy = "majors")
+    private Set<University> universities = new HashSet<>();
 }
