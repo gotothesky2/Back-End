f95e2543b3a2d2253c67ae546587c9102ed6b5cf
EarlyXS <shm02156@gmail.com>
2025-08-20 18:28:02 +0900

    ìwriting mistake issue lol

--- src/main/java/hackerthon/likelion13th/canfly/global/config/WebConfig.java
+++ src/main/java/hackerthon/likelion13th/canfly/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 애플리케이션의 모든 API 경로에 대해 CORS 설정을 적용합니다.
-                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "https://localhost:3000", "https://can-fly.netlify.app/") // 로컬 환경의 Swagger UI
+                .allowedOrigins("http://localhost:8080", "http://127.0.0.1:8080", "http://localhost:3000", "http://127.0.0.1:3000", "https://can-fly.netlify.app/") // 로컬 환경의 Swagger UI
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // 허용할 HTTP 메서드를 지정합니다.
                 .allowedHeaders("*") // 모든 HTTP 헤더를 허용합니다.
                 .allowCredentials(true); // 쿠키 및 인증 정보를 포함한 요청을 허용합니다.
